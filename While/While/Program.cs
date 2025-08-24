f35fd4632f929b38e75199246e5c19c3b19dd955
Valdinei Antunes <valdineifilho21@gmail.com>
2025-08-24 19:59:37 -0300

    While

---
+++ While/While/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace While {
+
+    class Program {
+
+        static void Main(string[] args) {
+
+            int i = 0;
+
+            while (i <= 10)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+        
+        }
+    }
+
+}
