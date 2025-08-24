e6c60731427923d1c65d763f31a5d596e11ac196
Valdinei Antunes <valdineifilho21@gmail.com>
2025-08-24 20:04:56 -0300

    Do While

---
+++ Do While/Do While/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DoWhile {
+
+    class Program {
+
+        static void Main(string[] args) {
+
+            int numero;
+
+            do {
+
+                Console.WriteLine("Digite um número: ");
+                 numero = int.Parse(Console.ReadLine());
+
+
+
+            } while (numero != 10);
+
+        }
+    
+    }
+
+}
