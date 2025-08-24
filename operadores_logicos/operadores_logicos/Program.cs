1c03ff56e1398b68b45336de99eaeaaf8028e945
Valdinei Antunes <valdineifilho21@gmail.com>
2025-08-24 18:58:46 -0300

    operadores

---
+++ operadores_logicos/operadores_logicos/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace operadores_logicos {
+
+    class Program {
+
+        public static void Main(string[] args) { 
+            
+            // operadores aritméticos
+
+            int a, b;
+
+            Console.Write("Digite ó primeiro número: ");
+            a = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o segundo número: ");
+            b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Adição: " + (a + b)); //adição
+            Console.WriteLine("Subtração: " + (a - b) ); //subtração
+            Console.WriteLine("Multiplicação: " + ( a * b)); //multiplicação
+            Console.WriteLine("Divisão: " + (a / b)); //divisão
+            Console.WriteLine("Resto da divisão: " + (a % b)); //resto da divisão
+            Console.WriteLine("Incremento: " + (++a)); //incremento
+            Console.WriteLine("Decremento: " + (--b)); //decremento
+
+
+            //operadores de comparação
+
+            int x = 10 , y = 20;
+
+            Console.WriteLine( x == y ); // == igual à
+            Console.WriteLine( x != y ); // != diferente de
+            Console.WriteLine( x > y ); // > maior que
+            Console.WriteLine(x < y); // < menor que
+            Console.WriteLine(x >= y); // >= maior ou igual a
+            Console.WriteLine(x <= y); // <= menor ou igual a
+            
+
+            //operadores lógicos
+
+            bool c1 = true, c2 = false;
+
+            Console.WriteLine(c1 && c2); // && "E"
+            Console.WriteLine(c1 || c2); // || "OU"
+            Console.WriteLine(!c1); // ! "NÃO"
+
+            //operadores de atribuição
+
+            int c = 30;
+
+            c = 30; // = igual
+            c += 10; // += adição
+            c -= 10; // -= subtração
+            c *= 10; // *= multiplicação
+            c /= 10; // /= divisão
+            c %= 10; // &= resto da divisão
+
+        }
+    
+    }
+
+}
---
+++ variaveis/variaveis/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+
+// tipos de variaveis
+int idade = 19;
+string nome = "Valdinei";
+double salario = 12500.00;
+bool estudante = true;
+char sexo = 'M';
+
+//constante
+const double pi = 3.14;
+
+//como exibibir as variaveis
+Console.WriteLine(salario);
+
+Console.WriteLine("Meu nome é: " + nome + " || " + "Minha idade é: " + idade);
+
+//escopo das variaveis
+
+
+    int idade2 = 20;
+    Console.WriteLine(idade2);
+
+Console.Write("Digite seu nome: ");
+string nome2 = Console.ReadLine();
+
+Console.WriteLine(nome2);
+
+Console.Write("Digite um número: ");
+int numero = int.Parse(Console.ReadLine());
+
+Console.Write("Digite outro número: ");
+int numero2 = int.Parse(Console.ReadLine());
+
+int resultado = numero + numero2;
+Console.WriteLine("O resultado da soma é: " + resultado);
