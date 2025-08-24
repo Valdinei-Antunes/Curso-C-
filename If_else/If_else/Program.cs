fb35815ea03b12a935648d00d3d60db65a1a4b07
Valdinei Antunes <valdineifilho21@gmail.com>
2025-08-24 19:20:29 -0300

    If e Else

---
+++ If_else/If_else/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+
+namespace If_Else {
+
+    class Program { 
+    
+    static void Main(String[] args)
+        {
+            // condicionais 
+
+            Console.Write("Digite a nota 1: ");
+            double nota1 = Double.Parse(Console.ReadLine());
+
+            Console.Write("Digite a nota 2: ");
+            double nota2 = Double.Parse(Console.ReadLine());
+
+            Console.Write("Digite a nota 3: ");
+            double nota3 = Double.Parse(Console.ReadLine());
+
+            double media = (nota1 + nota2 + nota3 ) / 3;
+
+            Console.WriteLine("Sua média é: " + media);
+
+            if (media >= 7)
+            {
+                Console.WriteLine("Aprovado");
+
+            }
+            else if (media >= 5 && media < 7)
+            {
+                Console.WriteLine("Recuperação");
+            }
+            else {
+                Console.WriteLine("Reprovado");
+            }
+        }
+    
+    }
+
+}
