a69dfe4f1357dd1f14d4967b05e2f5b74dd42c82
Valdinei Antunes <valdineifilho21@gmail.com>
2025-08-24 19:53:47 -0300

    For

---
+++ for/for/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace For
+{
+    class Program {
+    
+    static void Main(string[] args) {
+
+            Console.Write("Digite um número para fazer a tabuada até 10:");
+            int numero = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i <= 10; i++)
+            {
+                int resultado = numero * i;
+                Console.WriteLine("" + numero + " * " + "" + i + " = " + resultado);
+            }
+        
+        }
+            
+            }
+}
